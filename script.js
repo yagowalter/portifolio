28623f21aa26ccf2de6b6d605ded0289d8947551
Yago Walter <yago.walter_7@hotmail.com>
2023-10-06 23:06:22 -0300

    Consertado script de escrevendo

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function escrevendoLetra() {
-    function ativaLetra(elemento) {
-        const arrayTexto = elemento.innerHTML.split('');
-        elemento.innerHTML = '';
-        arrayTexto.forEach((letra, i) => {
-            setTimeout(() => {
-                elemento.innerHTML += letra;
-            }, 75 * i);
-        })
-    }
-
-    const titulo = document.querySelector('.digitando');
-
-    ativaLetra(titulo);
 }
+function ativaLetra(elemento) {
+    const arrayTexto = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    arrayTexto.forEach((letra, i) => {
+        setTimeout(() => {
+            elemento.innerHTML += letra;
+        }, 75 * i);
+    })
+}
+
+const titulo = document.querySelector('.digitando');
+
+ativaLetra(titulo);
 
 
 const ativaMenu = document.querySelector('.fa-bars');
