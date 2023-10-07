023fa3979035c1522a996340b0b5da592ad5798c
Yago Walter <yago.walter_7@hotmail.com>
2023-10-06 22:10:32 -0300

    Adicionado script simples para o header

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-function ativaLetra(elemento){
-    const arrayTexto = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
-    arrayTexto.forEach((letra,i)=>{
-        setTimeout(() => {
-            elemento.innerHTML += letra;
-        }, 75 * i);
-    })
+function escrevendoLetra() {
+    function ativaLetra(elemento) {
+        const arrayTexto = elemento.innerHTML.split('');
+        elemento.innerHTML = '';
+        arrayTexto.forEach((letra, i) => {
+            setTimeout(() => {
+                elemento.innerHTML += letra;
+            }, 75 * i);
+        })
+    }
+
+    const titulo = document.querySelector('.digitando');
+
+    ativaLetra(titulo);
 }
 
-const titulo = document.querySelector('.digitando');
 
-ativaLetra(titulo);
+const ativaMenu = document.querySelector('.fa-bars');
+
+ativaMenu.addEventListener('click', () => {
+    ativaMenu.classList.toggle('fa-x')
+});
